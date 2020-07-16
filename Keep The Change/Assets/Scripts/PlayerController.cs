f037c9dada6830668d03c1ad1120fc0710b6e073
Desca <scavanna@live.com>
2020-07-16 19:58:14 +0200

    Primera entrada
    Prueba

---
+++ Keep The Change/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float startSpeed = 5f;
+    float horizontalInput;
+    float verticalInput;
+
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+    void PlayerMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * Time.deltaTime * startSpeed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * startSpeed * verticalInput);
+    }
+}
