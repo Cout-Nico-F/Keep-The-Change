c5de373c68e4baab1597e57a161384c1fb59422c
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-07-17 15:20:27 -0300

    New Sprites

---
+++ Keep The Change/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Cree este script para probar un error en el animator 
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
