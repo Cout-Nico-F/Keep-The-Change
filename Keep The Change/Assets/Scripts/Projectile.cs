2fbb79f78d7601270a06905fe3cb8d8ce0e066ef
Desca <scavanna@live.com>
2020-07-19 11:42:59 +0200

    Projectile Prefab and Shooting redesign

---
+++ Keep The Change/Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Vector3 target;
+    Vector3 direction;
+    [SerializeField] float speed = 5.0f;
+    void Start()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target.z = 0;
+        direction = (target - transform.position).normalized;
+
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.Translate(direction * step);
+    }
+}
--- Keep The Change/Assets/Scripts/Shooting.cs
+++ Keep The Change/Assets/Scripts/Shooting.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     [SerializeField] float fireRate;
-    [SerializeField] float nextFire;
-    [SerializeField] int damage = 10;
-    [SerializeField] LayerMask toHit;
+    float nextFire;
     [SerializeField] Transform fireSource;
-    [SerializeField] float rayCastDist = 0f;
+    [SerializeField] GameObject bullet;
     void Start()
     {
         if (fireSource == null)
@@ -39,15 +37,7 @@ public class Shooting : MonoBehaviour
 
     void Shoot()
     {
-        Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-        Vector2 sourcePos = new Vector2(fireSource.position.x, fireSource.position.y);
-        RaycastHit2D hit = Physics2D.Raycast(sourcePos, mousePos - sourcePos, rayCastDist, toHit);
-        Debug.DrawLine(sourcePos, (mousePos - sourcePos) * rayCastDist, Color.green);
-        if (hit.collider != null)
-        {
-            Debug.DrawLine(sourcePos, hit.point, Color.red);
-            Debug.Log("We hit " + hit.collider.name + " for " + damage + "damage.");
-        }
-
+        Debug.Log("Shoot");
+        Instantiate(bullet, transform.position, bullet.transform.rotation);
     }
 }
