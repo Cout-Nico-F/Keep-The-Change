ab9056bdb02a763b1c3c5b5ad3e51f3c9af4f8f4
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-07-22 19:08:00 -0300

    improving the sorting system

--- Keep The Change/Assets/Scripts/LayerSorter.cs
+++ Keep The Change/Assets/Scripts/LayerSorter.cs
@@ -6,6 +6,9 @@ public class LayerSorter : MonoBehaviour
 {
     private SpriteRenderer parentRenderer;
     private int baseOrderInLayer;
+    private List<DynamicSorter> dynamicSorters = new List<DynamicSorter>();
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,12 +26,34 @@ public class LayerSorter : MonoBehaviour
     {
         if (collision.tag == "DynamicSorter")
         {
-            parentRenderer.sortingOrder = collision.GetComponent<SpriteRenderer>().sortingOrder - 1;
+            DynamicSorter dyn = collision.GetComponent<DynamicSorter>();
+
+            if(dynamicSorters.Count == 0 || dyn.MySpriteRenderer.sortingOrder -1 < parentRenderer.sortingOrder)
+            {
+                parentRenderer.sortingOrder = dyn.MySpriteRenderer.sortingOrder - 1;
+            }
+
+            dynamicSorters.Add(dyn);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        parentRenderer.sortingOrder = baseOrderInLayer;
+        if(collision.tag == "DynamicSorter")
+        {
+            DynamicSorter dyn = collision.GetComponent<DynamicSorter>();
+            dynamicSorters.Remove(dyn);
+            if(dynamicSorters.Count == 0)
+            {
+                parentRenderer.sortingOrder = baseOrderInLayer;
+            }
+            else
+            {
+                dynamicSorters.Sort();
+                parentRenderer.sortingOrder = dynamicSorters[0].MySpriteRenderer.sortingOrder - 1;
+            }
+            
+        }
+        
     }
 }
