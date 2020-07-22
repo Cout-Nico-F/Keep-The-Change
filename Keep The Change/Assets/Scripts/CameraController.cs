6a446b4d4d7e2a530830f2117f18d90d05b5bf21
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-07-22 18:33:26 -0300

    Solving sorting layer problem

--- Keep The Change/Assets/Scripts/CameraController.cs
+++ Keep The Change/Assets/Scripts/CameraController.cs
@@ -12,7 +12,8 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
-        
+        GetComponent<Camera>().transparencySortMode = TransparencySortMode.CustomAxis;
+        GetComponent<Camera>().transparencySortAxis = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
---
+++ Keep The Change/Assets/Scripts/LayerSorter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSorter : MonoBehaviour
+{
+    private SpriteRenderer parentRenderer;
+    private int baseOrderInLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentRenderer = transform.parent.GetComponent<SpriteRenderer>();
+        baseOrderInLayer = parentRenderer.sortingOrder;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "DynamicSorter")
+        {
+            parentRenderer.sortingOrder = collision.GetComponent<SpriteRenderer>().sortingOrder - 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        parentRenderer.sortingOrder = baseOrderInLayer;
+    }
+}
