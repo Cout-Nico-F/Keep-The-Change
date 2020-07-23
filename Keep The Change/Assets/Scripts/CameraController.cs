9c8fd1de7db376c215b1ac5659a38c4c9c0e81df
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-07-23 00:06:37 -0300

    Fade Effect for music loop

--- Keep The Change/Assets/Scripts/CameraController.cs
+++ Keep The Change/Assets/Scripts/CameraController.cs
@@ -7,13 +7,16 @@ public class CameraController : MonoBehaviour
     [SerializeField] GameObject player; // GO for the camera to follow
     [SerializeField] Vector3 offset; // offset for the Camera Following the GO
     [SerializeField] bool followPlayer = false; // checkbox in the Inspector
-
+    [SerializeField] float setVolume;
     [SerializeField] Vector3 moveScreenRight = new Vector3 (28,0,0);
 
     void Start()
     {
         GetComponent<Camera>().transparencySortMode = TransparencySortMode.CustomAxis;
         GetComponent<Camera>().transparencySortAxis = new Vector3(0, 1, 0);
+        setVolume = AudioListener.volume;
+        AudioListener.volume = 0;
+
     }
 
     // Update is called once per frame
@@ -23,7 +26,22 @@ public class CameraController : MonoBehaviour
         { 
             if (player != null) transform.position = player.transform.position + offset;
         }
+
+        if (AudioListener.volume < setVolume)
+        {
+            AudioListener.volume += 0.0085f;
+        }
     }
+/*
+    float elapsedTime = 0;
+    float currentVolume = AudioListener.volume;
+ 
+        while(elapsedTime<delay) {
+            elapsedTime += Time.deltaTime;
+            AudioListener.volume = Mathf.Lerp(currentVolume, 0, elapsedTime / delay);
+            yield return null;
+*/
+
 
     public void MoveCameraRight()
     {
