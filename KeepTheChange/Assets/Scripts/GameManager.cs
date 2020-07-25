e47cb7ce38e88f9f65f9042543670a1e06e82717
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-07-25 20:29:11 -0300

    Pause menu fix

--- KeepTheChange/Assets/Scripts/GameManager.cs
+++ KeepTheChange/Assets/Scripts/GameManager.cs
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        Transform T = ReferenceUI.Instance.MainCanvas.transform.Find("PauseMenu");
+        pauseMenuUI = T.gameObject;
         gameOver = false;
     }
 
