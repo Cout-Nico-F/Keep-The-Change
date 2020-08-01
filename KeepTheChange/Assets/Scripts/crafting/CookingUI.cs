99032d3e9d955b6f8f26b761ad95e1e75c0cd43b
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-08-01 19:44:25 -0300

    Thinking on this as a way to make more recipes

--- KeepTheChange/Assets/Scripts/crafting/CookingUI.cs
+++ KeepTheChange/Assets/Scripts/crafting/CookingUI.cs
@@ -7,11 +7,11 @@ public class CookingUI : CraftingUI
   {
     if (Input.GetKeyDown(KeyCode.C))
     {
-      this.TryCook();
+      this.DemoRecipe();
     }
   }
 
-  private bool TryCook()
+  private bool DemoRecipe()
   {
     ItemType demoTypeA = ItemType.Carrot;
     ItemType demoTypeB = ItemType.WoodenBowl;
@@ -23,6 +23,24 @@ public class CookingUI : CraftingUI
       this.playerInventoryUI.SubtractItem(new Item(demoTypeB, 1));
       // add crafted item to player inventory
       this.playerInventoryUI.AddItem(new Item(ItemType.CarrotSoup, 1));
+    }
+    else
+    {
+      print("player inventory missing items");
+    }
+
+    return true;
+  }
+
+  private bool TryCook(ItemType ingredient1, ItemType ingredient2, ItemType result)
+  {
+    if (this.hasItems(ingredient1, ingredient2))
+    {
+      // remove those items from player inventory
+      this.playerInventoryUI.SubtractItem(new Item(ingredient1, 1));
+      this.playerInventoryUI.SubtractItem(new Item(ingredient2, 1));
+      // add crafted item to player inventory
+      this.playerInventoryUI.AddItem(new Item(result, 1));
     }
     else
     {
